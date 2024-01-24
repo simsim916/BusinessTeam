8e043cbf61c933dac5eb8c269a2366e04cdc5a77
moon_serface <simsim916@naver.com>
2024-01-24 12:06:26 +0900

    24-01-24

--- js/nav2.js
+++ js/nav2.js
@@ -4,11 +4,17 @@ function openSideBar() {
         topBar.style.paddingLeft = "15px"
         sideBarButton.children[0].style.transform = 'rotateY(0deg)'
         sideBarButton.children[0].style.left = '1px'
+        for (div of containers) {
+            div.style.transform = "translateX(-40px)"
+        }
     } else {
         sideBar.style.transform = "translateX(0)";
         topBar.style.paddingLeft = "95px"
         sideBarButton.children[0].style.left = '3px'
         sideBarButton.children[0].style.transform = 'rotateY(180deg)'
+        for (div of containers) {
+            div.style.transform = "translateX(0)"
+        }
     }
     sideBarStatus = !sideBarStatus;
 }
@@ -23,4 +29,4 @@ function closeDetail() {
     sideBarDetailFirst.style.display = "none"
     sideBarDetailSecond.style.display = "none"
     sideBarDetailFirst.style.transform = "translateX(0)";
-}
+};
