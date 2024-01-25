f241f04292bcca617c69ebc99a50136e299bc04e
JYH <dydgusc66@naver.com>
2024-01-25 18:12:18 +0900

    24-01-25 js Test

--- company/Javascript/orderPageT.js
+++ company/Javascript/orderPageT.js
@@ -1,9 +1,8 @@
 
 function selectAll() {
-    const checkAll = document.querySelector('#checkAll');
-    const isChecked = checkAll.checked;
+    const checkboxes = document.getElementsByClassName(".chk");
     // console.log(checkAll);
-    if (isChecked) {
+    if (checkAll.checked) {
         const checkboxes = document.querySelectorAll('.chk');
 
         for (const a of checkboxes) {
@@ -16,5 +15,25 @@ function selectAll() {
             a.checked = false;
         }
     }
-    console.log(isChecked);
+}
+
+function uncheckedAllBox() {
+    checkAll.checked = false;
+}
+
+function insertData() {
+    const orderList = document.querySelector('#insertRowYH');
+    const makeDiv = document.createElement("div");
+
+    for (let index = 0; index < 12; index++) {
+        if (index == 0) {
+            orderList.appendChild(makeDiv);
+            const makeInput = makeDiv.createElement("input");
+            makeInput.className = "chk";
+        } else {
+            
+        }
+        orderList.appendChild(makeDiv);
+    }
+    orderList.appendChild(makeDiv);
 }
