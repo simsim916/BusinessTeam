3306ec07ae5850adcdd09cf1039a68b79cf1b17e
JYH <dydgusc66@naver.com>
2024-02-05 15:46:45 +0900

    24-02-05 (시간,쿠폰공간,상품영역,호버)

---
+++ tomatoFarmA/resources/js/eventPage.js
@@ -0,0 +1,41 @@
+'use strict'
+
+// ===========================================================
+// ===========================================================
+const couponBox2 = document.querySelector('#couponBox2');
+
+// =============== 완성하고 HTML 하단으로 빼기 ================
+// ===========================================================
+
+
+// couponBox2.addEventListener('change', () => {
+
+//     couponBox2.animate([
+//         { transform: 'translate(0, 0);' },
+//         { transform: 'translate(0, -15px)' },
+//         { transform: 'translate(0, -8px)' },
+//         { transform: 'translate(0, -4px)' },
+//         { transform: 'translate(0, -6px)' },
+//         { transform: 'translate(0, -3px)' },
+//         { transform: 'translate(0, -6px)' },
+//         { transform: 'translate(0, -3px)' },
+//         { transform: 'translate(0, -1px)' },
+//         { transform: 'translate(0, 0px)' },
+//     ], {
+//         duration: 1000,
+//         delay: 700,
+//         // 무한반복 하고싶은데 그게 안돼
+//     });
+// });
+
+couponBox2.addEventListener('click', (event) => {
+    couponBox2.innerHTML = "";
+    couponBox2.style.border = "none";
+    
+
+});
+
+
+
+
+
