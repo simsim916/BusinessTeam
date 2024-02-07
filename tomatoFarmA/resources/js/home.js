752ff5fdf857b833d2bc72e8c7cc52402428a558
moon_serface <simsim916@naver.com>
2024-02-07 11:39:02 +0900

    24-02-06

--- tomatoFarmA/resources/js/home.js
+++ tomatoFarmA/resources/js/home.js
@@ -1,9 +1,11 @@
 let hides = document.getElementsByClassName("hide");
-const saleSlideBtn = document.getElementById("secondContainerRightBtn").children[0];
+const slideBox = document.getElementsByClassName("slideBox");
+const secondSlideBtn = document.getElementById("secondSlideBtn");
+let secondContainerSlideMargin = 0 , 
+    thirdContainerSlide0Margin = 0;
 
 window.addEventListener("scroll", () => {
     for (e of hides) {
-        a(e.getBoundingClientRect().top - window.innerHeight)
         if (e.getBoundingClientRect().top < 100 + window.innerHeight) {
             e.style.opacity = "1";
             e.style.marginTop = "30px";
@@ -11,3 +13,45 @@ window.addEventListener("scroll", () => {
     }
 });
 
+function secondContainerSlideLeftBtn() {
+    document.getElementById('secondSlideBtnSelected').removeAttribute("id");
+    if (secondContainerSlideMargin < 2200) {
+        secondContainerSlideMargin += 440;
+    }
+    secondSlideBtn.children[`${5 - secondContainerSlideMargin / 440}`].setAttribute("id", "secondSlideBtnSelected")
+    slideBox[0].style.marginLeft = `${secondContainerSlideMargin}px`;
+}
+
+function secondContainerSlideRightBtn() {
+    document.getElementById('secondSlideBtnSelected').removeAttribute("id");
+    if (secondContainerSlideMargin > -2200) {
+        secondContainerSlideMargin -= 440;
+    }
+    secondSlideBtn.children[`${5 - secondContainerSlideMargin / 440}`].setAttribute("id", "secondSlideBtnSelected");
+    slideBox[0].style.marginLeft = `${secondContainerSlideMargin}px`;
+}
+
+function secondContainerSlideBtn(event) {
+    let target = event.target;
+    if (event.target.id != 'secondSlideBtn') {
+        document.getElementById('secondSlideBtnSelected').removeAttribute("id");
+        event.target.setAttribute("id", "secondSlideBtnSelected");
+        let index = 0;
+        for (a of secondSlideBtn.children) {
+            if (target == a) {
+                break;
+            }
+            ++index;
+        }
+        secondContainerSlideMargin = 2200 - (440 * index);
+        slideBox[0].style.marginLeft = `${secondContainerSlideMargin}px`;
+    }
+}
+
+function thirdContainerSlideRightBth(){
+    if (thirdContainerSlide0Margin > -660){
+        thirdContainerSlide0Margin -= 220;
+        a(thirdContainerSlide0Margin)
+        slideBox[1].style.marginLeft = `${thirdContainerSlide0Margin}px`;
+    }
+}
