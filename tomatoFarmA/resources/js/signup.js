92ff9fbc914c9e60ce52e60f831fa9c408bb1fbe
ksm <tnal7tndus@naver.com>
2024-02-14 15:17:07 +0900

    2024.02.14. 회원가입완료

--- tomatoFarmA/resources/js/signup.js
+++ tomatoFarmA/resources/js/signup.js
@@ -4,6 +4,10 @@ const passwordBox = document.getElementById('passwordBox');
 const nameBox = document.getElementById('nameBox');
 const phonenumberBox = document.getElementById('phonenumberBox');
 const errorBox = document.getElementById('errorBox');
+const addressBox = document.getElementById('addressBox');
+const emailBox = document.getElementById('emailBox');
+const genderBox = document.getElementById('genderBox');
+const birthdayBox = document.getElementById('birthdayBox');
 
 const emailSelectBox = document.getElementById("emailSelectBox");
 const emailWriteBox = document.getElementById("emailWriteBox");
@@ -24,11 +28,76 @@ function focusInputBox(event) {
     box.style.border = "2px solid #9B1B30";
 }
 
-function changeOpacity(event){
+function changeOpacity(event) {
     let box = event.target.closest('div');
-    for ( e of box.children){
-        e.style.opacity="1";
+    for (e of box.children) {
+        e.style.opacity = "1";
     }
+}
+function changeOpacityId(event) {
+    let box = event.target.closest('div');
+    for (e of box.children) {
+        e.style.opacity = "1";
+    }
+    if (event.which == 13) {
+        event.preventDefault();
+        passwordBox.children[1].focus();
+    }
+}
+function changeOpacityPw(event) {
+    let box = event.target.closest('div');
+    for (e of box.children) {
+        e.style.opacity = "1";
+    }
+    if (event.which == 13) {
+        event.preventDefault();
+        nameBox.children[1].focus();
+    }
+}
+function changeOpacityName(event) {
+    let box = event.target.closest('div');
+    for (e of box.children) {
+        e.style.opacity = "1";
+    }
+    if (event.which == 13) {
+        event.preventDefault();
+        phonenumberBox.children[1].focus();
+    }
+}
+function changeOpacityPn(event) {
+    let box = event.target.closest('div');
+    for (e of box.children) {
+        e.style.opacity = "1";
+    }
+    if (event.which == 13) {
+        event.preventDefault();
+        addressBox.children[1].focus();
+    }
+}
+function changeOpacityAddress(event) {
+    let box = event.target.closest('div');
+    for (e of box.children) {
+        e.style.opacity = "1";
+    }
+    if (event.which == 13) {
+        event.preventDefault();
+        emailBox.children[1].focus();
+    }
+}
+function changeOpacityEmail(event) {
+    let box = event.target.closest('div');
+    for (e of box.children) {
+        e.style.opacity = "1";
+    }
+    if (event.which == 13) {
+        event.preventDefault();
+        emailSelectBox.focus();
+    }
+}
+
+function changeOpacity2(event) {
+    event.target.closest('div').children[0].style.opacity = "1";
+    event.target.style.opacity = "1";
 }
 
 function checkId(event) {
@@ -37,20 +106,21 @@ function checkId(event) {
 
     if (value.length < 4 || value.length > 15) {
         idBox.style.border = "2px solid #FF3F3F";
+        idBox.style.borderBottom = "1px solid #FF3F3F";
         idBox.children[0].style.color = "#FF3F3F";
         document.getElementById('idError').innerHTML = `<i class="fa-solid fa-circle-exclamation"></i>&nbsp;&nbsp;아이디 : 4 ~ 15 글자 이하만 가능합니다.<br>`;
     } else if (value.replace(key, '').length > 0) {
         idBox.style.border = "2px solid #FF3F3F";
+        idBox.style.borderBottom = "1px solid #FF3F3F";
         idBox.children[0].style.color = "#FF3F3F";
         document.getElementById('idError').innerHTML = `<i class="fa-solid fa-circle-exclamation"></i>&nbsp;&nbsp;아이디 : 영문, 숫자, 특수문자(-, _)만 가능합니다.<br>`;
-    } else{
+    } else {
         idCheck = true;
         idBox.style.border = "2px solid #03C75A";
         idBox.style.borderBottom = "1px solid #03C75A";
         idBox.children[0].style.color = "#03C75A";
-        document.getElementById('idError').innerHTML='';
+        document.getElementById('idError').innerHTML = '';
     }
-        return null;
 }//checkId
 
 function checkPassword(event) {
@@ -59,59 +129,77 @@ function checkPassword(event) {
 
     if (value.length < 4 || value.length > 10) {
         passwordBox.style.border = "2px solid #FF3F3F";
+        passwordBox.style.borderTop = "1px solid #FF3F3F";
+        passwordBox.style.borderBottom = "1px solid #FF3F3F";
         passwordBox.children[0].style.color = "#FF3F3F";
         document.getElementById('pwError').innerHTML = `<i class="fa-solid fa-circle-exclamation"></i>&nbsp;&nbsp;비밀번호 : 4 ~ 10 글자 이하만 입력해주세요.<br>`;
     } else if (value.replace(key, '').length > 0) {
         passwordBox.style.border = "2px solid #FF3F3F";
+        passwordBox.style.borderTop = "1px solid #FF3F3F";
+        passwordBox.style.borderBottom = "1px solid #FF3F3F";
         passwordBox.children[0].style.color = "#FF3F3F";
         document.getElementById('pwError').innerHTML = `<i class="fa-solid fa-circle-exclamation"></i>&nbsp;&nbsp;비밀번호 : 영문, 숫자, 특수문자(!,@,#,$,%,^,&,*)만 가능합니다.<br>`;
-    } else if (value.replace(/[!-*.@]/gi, '').length > value.length) {
+    } else if (value.replace(/[!-*.@]/gi, '').length >= value.length) {
         passwordBox.style.border = "2px solid #FF3F3F";
+        passwordBox.style.borderTop = "1px solid #FF3F3F";
+        passwordBox.style.borderBottom = "1px solid #FF3F3F";
         passwordBox.children[0].style.color = "#FF3F3F";
         document.getElementById('pwError').innerHTML = `<i class="fa-solid fa-circle-exclamation"></i>&nbsp;&nbsp;비밀번호 : 특수문자(!,@,#,$,%,^,&,*)를 반드시 포함해주세요.<br>`;
     } else {
         pwCheck = true;
         passwordBox.style.border = "2px solid #03C75A";
+        passwordBox.style.borderTop = "1px solid #03C75A";
         passwordBox.style.borderBottom = "1px solid #03C75A";
         passwordBox.children[0].style.color = "#03C75A";
         document.getElementById('pwError').innerHTML = '';
     }
-        return null;
 }//checkPassword
 
 function checkName(event) {
     let value = event.target.value;
-    if (value.length < 1) {
+    if (value.length < 2 || value.length > 10) {
         nameBox.style.border = "2px solid #FF3F3F";
-        document.getElementById('nameError').innerHTML = `<i class="fa-solid fa-circle-exclamation"></i>&nbsp;&nbsp;이름 : 2글자 이상 입력하세요.<br>`;
-    } else if (value.replace(/[a-z.가-힣]/gi, '').length > 0){
+        nameBox.style.borderTop = "1px solid #FF3F3F";
+        nameBox.style.borderBottom = "1px solid #FF3F3F";
+        nameBox.children[0].style.color = "#FF3F3F";
+        document.getElementById('nameError').innerHTML = `<i class="fa-solid fa-circle-exclamation"></i>&nbsp;&nbsp;이름 : 2글자 이상 10글자 이하로 입력하세요.<br>`;
+    } else if (value.replace(/[a-z.가-힣]/gi, '').length > 0) {
         nameBox.style.border = "2px solid #FF3F3F";
+        nameBox.style.borderTop = "1px solid #FF3F3F";
+        nameBox.style.borderBottom = "1px solid #FF3F3F";
+        nameBox.children[0].style.color = "#FF3F3F";
         document.getElementById('nameError').innerHTML = `<i class="fa-solid fa-circle-exclamation"></i>&nbsp;&nbsp;이름은 한글, 영문만 입력하세요.<br>`;
     } else {
         nameCheck = true;
         nameBox.style.border = "2px solid #03C75A";
         nameBox.style.borderBottom = "1px solid #03C75A";
+        nameBox.style.borderTop = "1px solid #03C75A";
         nameBox.children[0].style.color = "#03C75A";
         document.getElementById('nameError').innerHTML = '';
     }
-    return null;
-    
+
 }//checkName
 
-function checkPhonenumber() {
+function checkPhonenumber(event) {
     let value = event.target.value;
-    if (value.length == 10) {
+    if (value.length < 10 || value.length > 13) {
+        phonenumberBox.style.border = "2px solid #FF3F3F";
+        phonenumberBox.style.borderTop = "1px solid #FF3F3F";
+        phonenumberBox.children[0].style.color = "#FF3F3F";
+        document.getElementById('pnError').innerHTML = `<i class="fa-solid fa-circle-exclamation"></i>&nbsp;&nbsp;전화번호는 9자리 ~ 12자리 숫자로 입력해주세요.<br>`;
+    } else if (value.replace(/[0-9]/gi, '').length > 0) {
+        phonenumberBox.style.border = "2px solid #FF3F3F";
+        phonenumberBox.style.borderTop = "1px solid #FF3F3F";
+        phonenumberBox.children[0].style.color = "#FF3F3F";
+        document.getElementById('pnError').innerHTML = `<i class="fa-solid fa-circle-exclamation"></i>&nbsp;&nbsp;전화번호는 숫자만 입력하세요.<br>`;
+    } else {
         phoneCheck = true;
         phonenumberBox.style.border = "2px solid #03C75A";
-        phonenumberBox.style.borderBottom = "1px solid #03C75A";
+        phonenumberBox.style.borderTop = "1px solid #03C75A";
         phonenumberBox.children[0].style.color = "#03C75A";
         document.getElementById('pnError').innerHTML = '';
-        } else {
-        phonenumberBox.style.border = "2px solid #FF3F3F";
-        document.getElementById('pnError').innerHTML = `<i class="fa-solid fa-circle-exclamation"></i>&nbsp;&nbsp;전화번호 : 다시 확인하세요.<br>`;
-        }
+    }
 }//checkPhonenumber
-
 
 function changeSelectBox(event) {
     if (event.target.value == "write") {
@@ -119,14 +207,20 @@ function changeSelectBox(event) {
         emailWriteBox.style.display = "inline-block";
         emailWriteBox.focus();
     }
-}
+}//changeSelectBox
 
 function selectGender(event) {
+    let value = event.target.value;
+    for (let e of event.target.closest('div').children) {
+        e.style.opacity = "1";
+        e.style.color = "black";
+    }
     if (document.getElementById('genderChecked') != null) {
         document.getElementById('genderChecked').removeAttribute("id");
     }
     event.target.closest('li').setAttribute("id", "genderChecked");
-}
+    birthdayBox.focus();
+}//selectGender
 
 
 
@@ -139,14 +233,14 @@ function selectGender(event) {
 //     if (!phoneCheck) { document.getElementById('phonenumberBox').innerHTML = '필수입력, 전화번호를 확인하세요.'; }
 
 //     if (idCheck && pwCheck && nameCheck && phoneCheck) {
-        
-//         if(confirm("가입을 진행하겠습니까?(YES : 확인 / NO : 취소)")){
+
+//         if (confirm("가입을 진행하겠습니까?(YES : 확인 / NO : 취소)")) {
 //             return true;
-//         }else {
+//         } else {
 //             alert("가입이 취소되었습니다.");
 //             return false;
 //         }
-//     }else {
+//     } else {
 //         return false;
-//         }
+//     }
 // }
