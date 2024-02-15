24bc4128f9e56986c0749b1746d3db9ea21756c1
JYH <dydgusc66@naver.com>
2024-02-15 14:29:01 +0900

    24-02-15

--- tomatoFarmA/resources/js/login.js
+++ tomatoFarmA/resources/js/login.js
@@ -1,7 +1,12 @@
-let formBox = document.getElementById('loginBox');
+// let formBox = document.getElementById('loginBox');
 let idInput = document.getElementById("id");
 let pwInput = document.getElementById("password");
-let eMessage = document.getElementById('errorMessage');
+let errorBox = document.getElementById('errorBox');
+let loginBt = document.getElementById('loginInBox').children[0];
+
+let id = "test";
+let pw = "123123!";
+
 
 function a(str) {
     console.log(str);
@@ -17,7 +22,7 @@ function SortLogin(ele) {
     idInput.value = "";
     pwInput.closest('div').style = "initial";
     pwInput.value = "";
-    eMessage.innerText = "";
+    errorBox.innerText = "";
     if (ele.innerText == "일반 로그인") {
         ele.style.backgroundColor = "rgb(146, 112, 112)";
         ele.style.borderBottom = "none";
@@ -37,45 +42,99 @@ function SortLogin(ele) {
 
 function focusInputBox(event) {
     let box = event.target.closest('div');
-    if (event.target.value == "") {
-        box.style.border = "2px solid #9B1B30";
-    }
-    for (e of box.children) {
-        e.style.opacity = "1";
-    }
-    if (box.getAttribute("id") == "passwordBox") {
-        idInput.closest('div').style.borderBottom = "1px solid #03C75A";
+
+    if (event.target == idInput) {
+        if (event.target.value == "") {
+            box.style.border = "2px solid #9B1B30";
+        } else if (event.target.value.length < 4 || event.target.value.length > 10) {
+            box.style.border = "2px solid #9B1B30";
+        } else {
+            box.style.border = "2px solid #03C75A";
+            box.style.borderBottom = "1px solid #03C75A";
+        }
+    } else {
+        if (event.target.value == "") {
+            box.style.border = "2px solid #9B1B30";
+        } else {
+            box.style.border = "2px solid #03C75A";
+            box.style.borderTop = "1px solid #03C75A";
+        }
     }
 }
 
 function keydownInput(event) {
     let box = event.target.closest('div');
-    if (event.target.value != "") {
-        box.style.border = "2px solid #03C75A";
+    let keyCode = event.keyCode;
+    if (event.target == idInput) {
+        if (event.target.value == "") {
+            box.style.border = "2px solid #9B1B30";
+        } else if (event.target.value.length < 4 || event.target.value.length > 10) {
+            box.style.border = "2px solid #9B1B30";
+        } else {
+            box.style.border = "2px solid #03C75A";
+            // box.style.borderBottom = "1px solid #03C75A";
+        }
+
+        if (keyCode == 13) {
+            pwInput.focus();
+        }
+    } else {
+        if (event.target.value == "") {
+            box.style.border = "2px solid #9B1B30";
+        } else {
+            box.style.border = "2px solid #03C75A";
+            box.style.borderTop = "1px solid #03C75A";
+        }
+
+        if (keyCode == 13) {
+            loginBt.focus();
+        }
     }
+
+
 }
+
 
 function focusOutInputBox(event) {
     let box = event.target.closest('div');
-    if (event.target.value != "") {
-        box.style.border = "2px solid #03C75A";
-    }
-}
-
-
-function loginCheck() {
-    if (idInput.value != "dydgusc66" && pwInput.value != "123123!") {
-        idInput.closest('div').style.border = "2px solid #FF3F3F";
-        idInput.closest('div').style.borderBottom = "1px solid #FF3F3F";
-        pwInput.closest('div').style.border = "2px solid #FF3F3F";
-        eMessage.innerText = "다 틀렸어";
-    } else if (idInput.value != "dydgusc66") {
-        idInput.closest('div').style.border = "2px solid #FF3F3F";
-        eMessage.innerText = "아이디 틀렸어";
-    } else if (pwInput.value != "123123!") {
-        pwInput.closest('div').style.border = "2px solid #FF3F3F";
-        eMessage.innerText = "패스워드 틀렸어";
+    if (event.target == idInput) {
+        if (event.target.value == "") {
+            box.style.border = "2px solid #ff3f3f";
+        } else if (event.target.value.length < 4 || event.target.value.length > 10) {
+            box.style.border = "2px solid #ff3f3f";
+            errorBox.innerText = "아이디는 4글자 이상 입니다."
+            
+        } else {
+            box.style.border = "2px solid #03C75A";
+            box.style.borderBottom = "1px solid #03C75A";
+        }
     } else {
-        eMessage.innerHTML = "성공";
+        if (event.target.value == "") {
+            box.style.border = "2px solid #9B1B30";
+        } else {
+            box.style.border = "2px solid #03C75A";
+            box.style.borderTop = "1px solid #03C75A";
+        }
     }
 }
+
+
+// function loginCheck() {
+//     if (idInput.value == id && pwInput.value == pw) {
+//         errorBox.innerText = "성공";
+//     } else if (idInput.value != id && pwInput.value == pw) {
+//         errorBox.innerText = "아이디가 틀렸습니다."
+//         idInput.closest('div').style.border = "2px solid #9B1B30";
+//         idInput.closest('div').style.borderBottom = "1px solid #9B1B30";
+//     } else if (idInput.value == id && pwInput.value != pw) {
+//         errorBox.innerText = "비밀번호가 틀렸습니다."
+//         pwInput.closest('div').style.border = "2px solid #9B1B30";
+//         pwInput.closest('div').style.borderTop = "1px solid #9B1B30";
+//     } else {
+//         errorBox.innerText = "아이디,비밀번호가 틀렸습니다."
+//         idInput.closest('div').style.border = "2px solid #9B1B30";
+//         idInput.closest('div').style.borderBottom = "1px solid #9B1B30";
+//         pwInput.closest('div').style.border = "2px solid #9B1B30";
+//         pwInput.closest('div').style.borderTop = "1px solid #9B1B30";
+//     }
+// }
