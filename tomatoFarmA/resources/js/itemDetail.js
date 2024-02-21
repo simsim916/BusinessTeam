c927abfd8e8616751b8d58abbfbc1c4ccd2d9360
ksm <tnal7tndus@naver.com>
2024-02-21 18:10:13 +0900

    2024.02.21

--- tomatoFarmA/resources/js/itemDetail.js
+++ tomatoFarmA/resources/js/itemDetail.js
@@ -34,11 +34,31 @@ function count(event, type) {
 function sumTotal(value) {
     const title6Value = document.getElementById('title6').innerText.replace('원', '');
     const priceBox = document.getElementById('price');
-    priceBox.children[0].innerText = `${value * title6Value} 원`
+    priceBox.children[0].innerText = `${value * title6Value} 원`;
 }
 
 function inputCount(event) {
     const inputBox = document.getElementById('inputCount');
     const countBox = document.getElementById('price');
     countBox.children[1].innerText = inputBox;
+}
+
+
+function showItemDetail(ele) {
+    let itemIntro = ele.previousElementSibling;
+    console.log(ele.children[0])
+    if (itemIntro.classList.contains('heightAuto'))
+        itemIntro.classList.remove('heightAuto');
+    else {
+        itemIntro.classList.add('heightAuto');
+        introItemBtn.innerHTML = `상품정보 접기<i class="fa-solid fa-chevron-up"></i>`;
+    }
+}
+
+function reivewDetail(event) {
+    event.stopPropagation();
+    const reviewImg = document.getElementsByClassName('reviewDetail').children[2];
+    reviewImg.style.width = 100;
+    reviewImg.style.height = 100;
+
 }
