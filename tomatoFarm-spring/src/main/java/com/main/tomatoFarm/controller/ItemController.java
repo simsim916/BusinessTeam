e22cca105d5bf32e399e86a66e7c65ceb23105db
moon_serface <simsim916@naver.com>
2024-03-06 15:46:29 +0900

    24-03-06

--- tomatoFarm-spring/src/main/java/com/main/tomatoFarm/controller/ItemController.java
+++ tomatoFarm-spring/src/main/java/com/main/tomatoFarm/controller/ItemController.java
@@ -14,7 +14,6 @@ import com.main.tomatoFarm.service.KeywordService;
 import com.main.tomatoFarm.service.SortService;
 
 import lombok.AllArgsConstructor;
-import paging.PageMaker;
 
 @AllArgsConstructor
 @Controller
@@ -24,38 +23,7 @@ public class ItemController {
 	ItemService itemService;
 	SortService sortSerivce;
 	KeywordService keywordService;
-	PageMaker pageMaker;
 
-	@GetMapping("/list")
-	public void list(Model model, @RequestParam("keyword") String keyword) {
-		List<ItemDTO> list = itemService.selectItemListWhereKeyword(keyword);
-		
-		pageMaker.setSnoEno();
-		pageMaker.setTotalRowsCount(itemService.itemListCount(keyword));
-		
-		// 검색결과 개수
-		
-		
-		if(list == null) {
-			model.addAttribute("size", 0);
-		} else {
-			model.addAttribute("size", list.size());
-		}
-		
-		// 검색키워드 등록
-		if(keyword != null) {
-			keywordService.updateKeywordCnt(keyword);
-		}
-		
-		// 검색결과
-		model.addAttribute("list", list);
-		model.addAttribute("keyword", keyword);
-		model.addAttribute("brandList", itemService.selectBrandList());
-		model.addAttribute("sortList", sortSerivce.selectSortList());
-		model.addAttribute("sortbList", sortSerivce.selectSortbList());
-
-		
-	}
 	
 	@GetMapping("/detail")
 	public void detail(Model model , @RequestParam("code") int code) {
