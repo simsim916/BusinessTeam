a8436357104b20bd25685c5725fa7e4297274a5a
JYH <dydgusc66@naver.com>
2024-03-14 17:45:36 +0900

    테스트

--- tomatoFarm/src/main/java/com/example/demo/controller/HomeController.java
+++ tomatoFarm/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,15 +11,18 @@ import lombok.extern.log4j.Log4j2;
 @Controller
 @Log4j2
 public class HomeController {
-	
-
+	@Autowired
+	PasswordEncoder passwordEncoder;
 	
 	@GetMapping("/home")
 	public void home() {
 		
 	}
 
-	
+	@GetMapping("/test")
+	public void test() {
+		System.out.println(passwordEncoder.encode("1234"));
+	}
 	
 	
 }
