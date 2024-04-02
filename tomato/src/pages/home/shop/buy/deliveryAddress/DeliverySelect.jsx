5275574653b1d12824c89f906a0ed1edf2b0ac40
moon_serface <simsim916@naver.com>
2024-04-02 15:38:38 +0900

    24-04-02

--- tomato/src/pages/home/shop/buy/deliveryAddress/DeliverySelect.jsx
+++ tomato/src/pages/home/shop/buy/deliveryAddress/DeliverySelect.jsx
@@ -1,9 +1,23 @@
+import './DeliverySelect.css'
 
 const DeliverySelect = () => {
     return (
         <div id="deliverySelect">
-            <h4>배송지 변경</h4>
-            <div></div>
+            <div id="deliverySelectContainer">
+                <h4>배송지 변경</h4>
+                <div className="addDelivery">배송지 추가하기</div>
+                <div className="deliverySelectRow">
+                    <h4>우리집</h4>
+                    <p>홍길동 010-1234-1234</p>
+                    <p>경기도 성남시 분당구 돌마로 46 KR 5층</p>
+                    <div className='delivery_select'>
+                        <div>수정</div>
+                        <div>선택</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
+
+export default DeliverySelect;
