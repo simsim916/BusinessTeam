82e1a043dd485d2297a75dd02aeddc1a2b5ba7b6
ksm <tnal7tndus@naver.com>
2024-04-05 17:27:05 +0900

    2024.04.05

--- tomatoFarm/src/main/java/com/example/demo/domain/ChatBotDTO.java
+++ tomatoFarm/src/main/java/com/example/demo/domain/ChatBotDTO.java
@@ -24,3 +24,14 @@ public class ChatBotDTO {
 
 	private Integer user_level;
 }
+
+public class Hello{
+	public static int a = 100;
+	
+	public static void main(String[] args) {
+		public int b = 10;
+		
+		final double pi = 3.14;
+		System.out.println(a);
+	}
+}
