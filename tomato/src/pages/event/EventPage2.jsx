adc1e39763adf8b0917ad24ed6acefa8ab3c7286
ksm <tnal7tndus@naver.com>
2024-04-09 11:12:26 +0900

    2024.04.09. evntpage

--- tomato/src/pages/event/EventPage2.jsx
+++ tomato/src/pages/event/EventPage2.jsx
@@ -6,7 +6,7 @@ const EventPage2 = () => {
         <div>
             <img id="eventImgLogo" src={SERVER_RESOURCE + "/img/adimg/signup.jpg"} />
             <img id="eventImgLogo" src={SERVER_RESOURCE + "/img/adimg/review.jpg"} />
-            <h3 id="eventPageTitle"><i class="fa-solid fa-star"></i> SPECIAL CUPON<i class="fa-solid fa-star"></i></h3>
+            <h1 id="eventPageTitle"><i class="fa-solid fa-star"></i> SPECIAL CUPON<i class="fa-solid fa-star"></i></h1>
             <div id="eventCuponForm">
                 <div id="eventIMG">
                     <img id="joinCupon" src={SERVER_RESOURCE + "/img/adimg/coupon.png"} />
@@ -26,38 +26,52 @@ const EventPage2 = () => {
                     <div>로그인 시 자동지급<br></br><i class="fa-solid fa-circle-arrow-right"></i></div>
                 </div>
             </div>
-            
+            <hr></hr>
             <div id="levelPointForm">
-                <h3>토마토팜 등업 & 포인트 혜택</h3>
+                <h1>토마토팜 등업 & 포인트 혜택</h1>
                 <div id="levelPointBox">
                     <div id="level_Box">
-                        <div>레드등급</div>
-                        <div>20%</div>
-                        <span>최대 20%할인 혜택</span>
-                        <span>레드등급 혜택기간 등업일 ~ 1년</span>
+                        <div id="eventBoxTitle">레드등급</div>
+                        <div id="eventSaleBox">20%</div>
+                        <div>최대 20%할인 혜택</div>
+                        <div id="rankDay">레드등급 혜택기간<br></br>등업일 ~ 1년</div>
                     </div> 
                         <div id="levelPlusPoint"><i class="fa-solid fa-plus"></i></div>    
                     <div id="level_point">
-                        <div>포인트</div>
-                        <div>3만원</div>
-                        <span>20만원 이상 구매 시</span>
-                        <span>포인트 사용기간 혜택 신청일 ~ 2024.12.31</span>
+                        <div id="eventBoxTitle">포인트</div>
+                        <div id="eventSaleBox">3만원</div>
+                        <div>20만원 이상 구매 시</div>
+                        <div id="rankDay">포인트 사용기간혜택<br></br>신청일 ~ 2024.12.31</div>
                     </div>    
                 </div>
 
-                <div>
-                    <div>행사기간</div>
+                <div id="eventDayBox">
+                    <div id="eventBoxTitle">행사기간</div>
                     <span>2024.1.1 ~ 2024.12.31</span>
-                    <div>행사대상</div>
+                    <div id="eventBoxTitle">행사대상</div>
                     <span>상품 구매 내역 우수 회원</span>
-                    <div>행사내용</div>
+                    <div id="eventBoxTitle">행사내용</div>
                     <span>한 ID당 기간 내 1회 참여 가능</span>
-                    <div>참여방법</div>
+                    <div id="eventBoxTitle">참여방법</div>
                     <span>1. 쿠폰 다운로드 및 쿠폰번호 복사</span>
                     <span>2. 아래 쿠폰번호 입력란에 쿠폰번호 입력 후 혜택 받기</span>
                     <input type="text" placeholder="쿠폰번호 12자리를 입력해 주세요."></input>
                 </div>
             </div>
+            <hr></hr>
+            <div id="payEventBox">
+                <h1>추가 결제 혜택 안내</h1>
+                <h2>쿠폰 혜택과 중복 적용 가능</h2>
+                <div>
+                    <img id="reveiwCupon" src={SERVER_RESOURCE + "/img/eventimg/네이버페이.jpg"} />
+                    <img id="reveiwCupon" src={SERVER_RESOURCE + "/img/eventimg/카카오페이.jpg"} />
+                    <img id="reveiwCupon" src={SERVER_RESOURCE + "/img/eventimg/KB페이.jpg"} />
+                    <img id="reveiwCupon" src={SERVER_RESOURCE + "/img/eventimg/토스페이.jpg"} />
+                </div>
+            </div>
+
+
+
         </div>
 
 
