577aca8e2cff06656df99f6a45bdbe34b2bbe0b6
Moon Desk <simsim916@naver.com>
2024-04-20 20:39:52 +0900

    240420

--- tomatoFarm_server/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ tomatoFarm_server/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -8,16 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("redirect:/home");
-	}
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/home");
+    }
 
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://tomatofarm.shop") // 클라이언트의 도메인 주소
-				.allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
-				.allowedHeaders("*") // 허용할 HTTP 헤더
-				.allowCredentials(true); // 인증 정보 허용 여부
-	}
+    public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**").allowedOrigins("http://tomatofarm.shop") // 클라이언트의 도메인 주소
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000") // 클라이언트의 도메인 주소
+
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 허용할 HTTP 헤더
+                .allowCredentials(true); // 인증 정보 허용 여부
+    }
 
 }
