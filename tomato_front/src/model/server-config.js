10f2620a989646c28cb78fd68656dd3a08390ff6
Moon Desk <simsim916@naver.com>
2024-04-20 17:49:53 +0900

    24-04-20

--- tomato_front/src/model/server-config.js
+++ tomato_front/src/model/server-config.js
@@ -1,2 +1,5 @@
-export const SERVER_URL = 'http://localhost:8090';
-export const SERVER_RESOURCE = 'http://localhost:8090/resources';
+// export const SERVER_URL = 'http://localhost:8090';
+// export const SERVER_RESOURCE = 'http://localhost:8090/resources';
+
+export const SERVER_URL = 'http://15.164.8.231:8090';
+export const SERVER_RESOURCE = 'http://15.164.8.231:8090/resources';
