c723f3690cb805cb39ba0450a4d84814d6b340d3
ksm <tnal7tndus@naver.com>
2024-04-22 11:39:57 +0900

    2024.04.22

--- tomato_front/src/model/server-config.js
+++ tomato_front/src/model/server-config.js
@@ -1,5 +1,5 @@
 // export const SERVER_URL = 'http://localhost:8090';
 // export const SERVER_RESOURCE = 'http://localhost:8090/resources';
 
-export const SERVER_URL = 'https://api.tomatofarm.shop';
-export const SERVER_RESOURCE = 'https://api.tomatofarm.shop/resources';
+export const SERVER_URL = 'http://15.165.114.198:8090';
+export const SERVER_RESOURCE = 'https://15.165.114.198:8090/resources';
